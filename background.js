e8f5b3cda967964f2500f4cb089d915f911251b4
He4eT <He4eT@oddsquat.org>
2024-01-23 03:44:47 +0100

    pages/search: prevent history pollution

--- background.js
+++ background.js
@@ -3,15 +3,44 @@ browser.action.onClicked.addListener((() =>
     url: '/pages/search/search.html'
   })))
 
+/* */
+
+let registeredTabs = []
+
+const addRegisteredTab = (tabId) =>
+  registeredTabs = [tabId, ...registeredTabs]
+
+const removeRegisteredTab = (tabId) =>
+  registeredTabs = registeredTabs.filter((x) => x !== tabId)
+
+/* */
+
+browser.tabs.onRemoved.addListener((tabId) => {
+  console.log(tabId, registeredTabs)
+  if (!registeredTabs.includes(tabId)) {
+    return
+  }
+
+  removeRegisteredTab(tabId)
+
+  const timeout = () => new Promise((resolve) =>
+    setTimeout(resolve, 500))
+
+  timeout()
+    .then(() => browser.sessions.getRecentlyClosed({maxResults: 1}))
+    .then((x) => (console.log(x), x))
+    .then(([sessionInfo]) => browser.sessions.forgetClosedTab(
+      sessionInfo.tab.windowId,
+      sessionInfo.tab.sessionId))
+})
+
 browser.runtime.onMessage.addListener((message, sender) => {
   switch (message.action) {
     case 'closeSender':
-      Promise.resolve()
-        .then(() => browser.tabs.remove(sender.tab.id))
-        .then(() => browser.sessions.getRecentlyClosed({maxResults: 1}))
-        .then(([sessionInfo]) => browser.sessions.forgetClosedTab(
-          sessionInfo.tab.windowId,
-          sessionInfo.tab.sessionId))
+      browser.tabs.remove(sender.tab.id)
+      break
+    case 'registerSender':
+      addRegisteredTab(sender.tab.id)
       break
   }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,4 +1,5 @@
 import * as Store from './modules/store.js'
+import * as bridge from './modules/bridge.js'
 import * as dom from './modules/dom.js'
 import * as inputHandlers from './modules/inputHandlers.js'
 
@@ -12,5 +13,7 @@ const store = Store.init({
   },
 })
 
-void inputHandlers.attachInputHandlers(store)
+void bridge.registerTab()
 void dom.enableFaviconFallback()
+
+void inputHandlers.attachInputHandlers(store)
